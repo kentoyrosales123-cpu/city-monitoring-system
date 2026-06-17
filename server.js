32b0f2302dadb9aff1a520df275bc3562f97bdb8
kent <kentoyrosales123@gmail.com>
2026-06-17 17:51:59 +0800

    Fix Render static folder path

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ createDefaultAdmin();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
@@ -48,7 +48,7 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "login.html"));
+  res.sendFile(path.join(__dirname, "Public", "login.html"));
 });
 
 const PORT = process.env.PORT || 5000;
